4db13746af4f120c5e4b590c2d6b94b13f1d32f8
iamtanjil <tanjil4work@gmail.com>
2022-11-24 22:25:58 +0600

    dashboard layout complete

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../../Layout/DashboardLayout";
 import Main from "../../Layout/Main";
 import Blog from "../../Pages/Blog/Blog";
+import AllUser from "../../Pages/Dashboard/AllUser/AllUser";
+import MyOrders from "../../Pages/Dashboard/MyOrders/MyOrders";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
 
-const router = createBrowserRouter([{
+const router = createBrowserRouter([
+    {
     path: '/',
     element: <Main></Main>,
     children: [
@@ -26,7 +30,22 @@ const router = createBrowserRouter([{
             element: <SignUp></SignUp>
         },
     ]
-}]);
+},
+{
+    path:'/dashboard',
+    element: <DashboardLayout></DashboardLayout>,
+    children: [
+        {
+            path:'/dashboard/alluser',
+            element: <AllUser></AllUser>
+        },
+        {
+            path:'/dashboard/myorders',
+            element: <MyOrders></MyOrders>
+        },
+    ]
+}
+]);
 
 
 export default router
